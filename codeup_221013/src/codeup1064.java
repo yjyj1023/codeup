3e91b0967c2144bed40f2afd75295f120d7ef40f
yjyj1023 <lyj19961023@gmail.com>
2022-10-14 00:17:09 +0900

    코드업 1061~1070번

---
+++ codeup_221013/src/codeup1064.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class codeup1064 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        System.out.println((num1>num2 ? num2:num1)>num3 ? num3:(num1>num2 ? num2:num1));
+    }
+}
---
+++ codeup_221013/src/codeup1069.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class codeup1069 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        switch (str){
+            case "A" : System.out.println("best!!!");
+                break;
+            case "B" : System.out.println("good!!");
+                break;
+            case "C" : System.out.println("run!");
+                break;
+            case "D" : System.out.println("slowly~");
+                break;
+            default:
+                System.out.println("what?");
+                break;
+
+        }
+    }
+}
